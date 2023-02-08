0cd96c4dfb4404d3e831837ff5cc72cc9c2aad52
Grace Joh <graceehjoh@gmail.com>
2023-02-08 10:33:52 -0600

    test: refactor and add test for Car #paint

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,39 +2,51 @@ require 'rspec'
 require './lib/car'
 
 RSpec.describe Car do
-  it 'exists' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car).to be_a Car
+  before(:each) do
+    @car = Car.new("Ford Mustang", 1500, 36)
   end
 
-  it 'has a make' do
-    car = Car.new("Ford Mustang", 1500, 36)
+  describe '#initialize' do
+    it 'exists' do
+      expect(@car).to be_a Car
+    end
 
-    expect(car.make).to eq("Ford")
+    it 'has a monthly payment' do
+      expect(@car.monthly_payment).to eq(1500)
+    end
+
+    it 'has a loan length' do
+      expect(@car.loan_length).to eq(36)
+    end
+
+    it 'has a default color nil' do
+      expect(@car.color).to eq(nil)
+    end
   end
 
-  it 'has a model' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.model).to eq("Mustang")
+  describe '#make' do
+    it 'has a make' do
+      expect(@car.make).to eq("Ford")
+    end
   end
 
-  it 'has a monthly payment' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.monthly_payment).to eq(1500)
+  describe '#model' do
+    it 'has a model' do
+      expect(@car.model).to eq("Mustang")
+    end
   end
 
-  it 'has a loan length' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.loan_length).to eq(36)
+  describe '#total_cost' do
+    it 'has a total cost' do
+      expect(@car.total_cost).to eq(54000)
+    end
   end
 
-  it 'has a total cost' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.total_cost).to eq(54000)
+  describe '#paint!' do
+    it 'paints car given color' do
+      @car.paint!(:blue)
+      
+      expect(@car.color).to eq(:blue)
+    end
   end
 end
