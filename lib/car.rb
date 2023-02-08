60fcf3350aa00799364dae77ff53bcd82f4a71aa
Grace Joh <graceehjoh@gmail.com>
2023-02-08 10:21:31 -0600

    feat: add Car #make, #model, and #total_cost

--- lib/car.rb
+++ lib/car.rb
@@ -6,4 +6,16 @@ class Car
     @monthly_payment = monthly_payment
     @loan_length = loan_length
   end
+
+  def make
+    @make_model.split(' ').first
+  end
+
+  def model
+    @make_model.split(' ').last
+  end
+
+  def total_cost
+    @monthly_payment * @loan_length
+  end
 end
